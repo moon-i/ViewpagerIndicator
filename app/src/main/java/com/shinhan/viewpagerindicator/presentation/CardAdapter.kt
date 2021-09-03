6f0d3de6ae977134d4cdf460fa6793ab9b416aba
moon-i <tjans1102@gmail.com>
2021-09-03 16:20:00 +0900

    [VPI-5] CardAdapter 생성
    card list를 위한 cardAdapter을 생성합니다
    
    refs - https://github.com/moon-i/ViewpagerIndicator/issues/5

---
+++ app/src/main/java/com/shinhan/viewpagerindicator/presentation/CardAdapter.kt
@@ -0,0 +1,40 @@
+package com.shinhan.viewpagerindicator.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.shinhan.viewpagerindicator.databinding.CardHolderBinding
+
+//
+//  CardAdapter.kt
+//  ViewpagerIndicator
+//
+//  Created by Seomoon Choi on 9/2/21
+//
+
+class CardAdapter : RecyclerView.Adapter<CardAdapter.ViewHolder>() {
+    var items: List<String> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return ViewHolder(CardHolderBinding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ViewHolder(private val binding: CardHolderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(
+            item: String,
+        ) {
+            binding.data = item
+        }
+    }
+}
